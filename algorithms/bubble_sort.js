d2bebbebc5b228f2c5ee2b1141b1d8b0cab6f434
Nehl03 <164747103+Nehl03@users.noreply.github.com>
2026-01-14 10:52:30 +0530

    first commit

---
+++ algorithms/bubble_sort.js
@@ -0,0 +1,8 @@
+const arr=[...inputarray];
+const steps=[];
+let n = arr.length;
+for (let i = 0; i < n ; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+        
+        }
+    }
---
+++ engine/steps.js
@@ -0,0 +1,5 @@
+export const STEP_TYPES = {
+    COMPARE: 'COMPARE',
+    SWAP: 'SWAP',
+    MARK_FINAL: 'MARK_FINAL'
+};
